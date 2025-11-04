4bb2ca4bb1f15cfd179163393e28ffea363b0a8a
Gilson de lima <>
2025-11-04 09:51:04 -0300

    refactor: organizar estrutura de pastas

---
+++ src/dia1/ola-mundo.js
@@ -0,0 +1,5 @@
+console.log("Olá, mundo!");
+const nome = "Gilson";
+const cidade = "São Paulo";
+const hobbies = ['Ler','Programar','Correr'];
+console.log(`Olá! Sou ${nome}, moro em ${cidade}, e meus hobbies são: ${hobbies}!`);
---
+++ src/dia1/variaveis.js
@@ -0,0 +1,19 @@
+// String (texto)
+const mensagem = "Olá, JavaScript!";
+
+// Número
+const idade = 25;
+const preco = 19.99;
+
+// Booleano (verdadeiro/falso)
+const isDeveloper = true;
+
+// Array (lista)
+const frutas = ["maçã", "banana", "laranja"];
+
+// Objeto (dados estruturados)
+const pessoa = {
+  nome: "Joana",
+  idade: 30,
+  cidade: "São Paulo"
+};
